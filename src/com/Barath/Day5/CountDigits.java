5f6d76871c1478d6e3ea2aa8958cfec1371df657
Barath <barathbarath128@gmail.com>
2025-05-28 19:26:04 +0530

    Day4 - Assignment

---
+++ src/com/Barath/Day5/CountDigits.java
@@ -0,0 +1,18 @@
+package com.Barath.Day5;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        System.out.println("Number of digits : " + fun(num,0));
+    }
+    static int fun(int n,int count) {
+        if (n == 0) {
+            return count;
+        }
+        return fun(n/10,count+1);
+    }
+}
