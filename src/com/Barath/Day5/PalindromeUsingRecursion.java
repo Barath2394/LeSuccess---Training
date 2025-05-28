968fda94d39349cd01b72f2f29575620f3a3573b
Barath <barathbarath128@gmail.com>
2025-05-28 19:27:34 +0530

    Day4 - Assignment

---
+++ src/com/Barath/Day5/PalindromeUsingRecursion.java
@@ -0,0 +1,22 @@
+package com.Barath.Day5;
+
+import java.util.Scanner;
+
+public class PalindromeUsingRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        String str = sc.next();
+        boolean ans = isPalindrome(str,0,str.length()-1);
+        System.out.println(ans);
+    }
+    static boolean isPalindrome(String str,int left,int right) {
+        if (left >= right) {
+            return true;
+        }
+        if (str.charAt(left) != str.charAt(right)) {
+            return false;
+        }
+        return isPalindrome(str,left+1,right-1);
+    }
+}
