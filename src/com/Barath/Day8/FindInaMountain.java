1e5c2ffa3684e02755d9837c8bc41e84003238a6
Barath <barathbarath128@gmail.com>
2025-06-01 11:25:37 +0530

    Day8 - Assignment

---
+++ src/com/Barath/Day8/FindInaMountain.java
@@ -0,0 +1,8 @@
+package com.Barath.Day8;
+
+public class FindInaMountain {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/Barath/Day8/MountainArray.java
@@ -0,0 +1,5 @@
+package com.Barath.Day8;
+
+public interface MountainArray {
+
+}
