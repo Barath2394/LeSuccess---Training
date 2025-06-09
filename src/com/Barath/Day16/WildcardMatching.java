03fbe3d35bed4b2ad57a4e20a98acac0fa942f10
Barath <barathbarath128@gmail.com>
2025-06-09 09:51:30 +0530

    Day16 - Assignment

---
+++ src/com/Barath/Day16/WildcardMatching.java
@@ -0,0 +1,53 @@
+package com.Barath.Day16;
+
+import java.util.*;
+
+public class WildcardMatching {
+    public static boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true;
+
+        // Initialize dp[0][j] when pattern starts with '*'
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if (pc == sc || pc == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        String[][] testCases = {
+                {"aa", "a"},
+                {"aa", "*"},
+                {"cb", "?a"},
+                {"adceb", "*a*b"},
+                {"acdcb", "a*c?b"}
+        };
+
+        for (String[] testCase : testCases) {
+            String s = testCase[0];
+            String p = testCase[1];
+            boolean result = isMatch(s, p);
+            System.out.println("Input: s = \"" + s + "\", p = \"" + p + "\" ➞ " + result);
+        }
+    }
+}
+
